b23e6fcb366d54d7aa44887358c4ea11bd093664
Enes BİBER <lazenes@gmail.com>
2021-12-04 01:58:07 +0300

    beta versiyon
    Test Ediliyor

---
+++ GOTGHeaderLibParser/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace GOTGHeaderLibParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(854, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ayıkla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HeaderLib Dosya Yolu";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(637, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "E:\\SteamLibrary\\steamapps\\common\\Marvel\'s Guardians of the Galaxy";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(15, 341);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1129, 644);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(1150, 341);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(891, 644);
+            this.listBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Dizin:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1161, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Dosya Adı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "HeaderLib Dosyaları:";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(16, 140);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(477, 144);
+            this.listBox3.TabIndex = 8;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2065, 992);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "PC_HeaderLib Viever |LaZEnEs sinnerclownceviri.com";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ GOTGHeaderLibParser/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Principal;
+
+namespace GOTGHeaderLibParser
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			listBox3.Items.Clear();
+			AddHeaderLibs(textBox1.Text);
+		}
+		private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string yol = textBox1.Text + "\\runtime\\" + listBox3.SelectedItem.ToString();
+			listBox1.Items.Clear();
+			listBox2.Items.Clear();		
+			GetResourceIDs(yol);
+
+		}
+
+
+		private void AddHeaderLibs(string folderName)
+		{
+			string path = Path.Combine(textBox1.Text + "runtime", folderName);
+			
+			string[] files = Directory.GetFiles(path, "*.pc_headerlib*", SearchOption.AllDirectories);
+			int count = files.Length;
+
+			ListViewItem[] listViewItems = new ListViewItem[files.Length];
+
+			for (int i = 0; i < count; i++)
+			{
+				string fileName = new FileInfo(files[i]).Name;
+				listBox3.Items.Add(fileName);
+			}
+
+
+
+
+		}
+
+		private void GetResourceIDs(string path)
+		{
+			try
+			{
+
+				List<string> resourceIDs = new List<string>();
+				List<string> names = new List<string>();
+				resourceIDs.AddRange(FindResourceIDs("[[assembly:/", path));
+				resourceIDs = resourceIDs.Distinct().ToList();
+				int resourceIDsCount = resourceIDs.Count;
+				for (int i = 0; i < resourceIDsCount; i++)
+				{
+					if (resourceIDs[i].Contains("?"))
+					{
+						int startIndex = resourceIDs[i].IndexOf('?') + 2;
+						int count = resourceIDs[i].IndexOf('.', resourceIDs[i].IndexOf('?')) - resourceIDs[i].IndexOf('?') - 2;
+
+						string resourceName = resourceIDs[i].Substring(startIndex, count);	
+
+						names.Add(resourceName);
+					}
+				}
+
+				names = names.Distinct().ToList();
+				listBox1.Items.AddRange(resourceIDs.ToArray());
+				listBox2.Items.AddRange(names.ToArray());
+				
+
+
+
+			}
+			catch (IOException ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+
+			//MessageBox.Show("Bitti.", "GOTG PC_HeaderLib  Tool", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		private List<string> FindResourceIDs(string stringToFind, string path)
+		{
+			List<string> names = new List<string>();
+
+
+
+
+
+			using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+			{
+				using (BinaryReader binaryReader = new BinaryReader(fileStream))
+				{
+					byte[] ByteBuffer = File.ReadAllBytes(path);
+					byte[] StringBytes = Encoding.UTF8.GetBytes(stringToFind);
+
+					for (int i = 0; i <= (ByteBuffer.Length - StringBytes.Length); i++)
+					{
+						if (ByteBuffer[i] == StringBytes[0])
+						{
+							int j;
+							for (j = 1; j < StringBytes.Length && ByteBuffer[i + j] == StringBytes[j]; j++) ;
+
+							if (j == StringBytes.Length)
+							{
+								fileStream.Seek(i, SeekOrigin.Begin);
+
+								byte letter;
+								string fileName = "";
+
+								while ((letter = binaryReader.ReadByte()) != 0)
+								{
+									fileName += Encoding.Default.GetString(new byte[] { letter });
+								}
+
+								names.Add(fileName.Replace(stringToFind, ""));
+							}
+						}
+					}
+				}
+			}
+
+			return names;
+		}
+    }
+}
